ee9e0f3e69a9bbb9a2e90c38d75b8a034094affa
ls-5s <2408224899@qq.com>
2026-02-01 16:09:47 +0800

    feat: 新增用户表

--- test-1/server/db/schema/users.ts
+++ test-1/server/db/schema/users.ts
@@ -14,3 +14,20 @@ export const usersTable = p.sqliteTable("users", {
   // 创建时间
   createdAt: p.integer("created_at", { mode: "timestamp" }),
 });
+// export type Message = p.InferModel<typeof messagesTable>;
+// export type NewMessage = p.InferModel<typeof messagesTable, "insert">;
+// export const loginTable = p.sqliteTable("login", {
+//   id: p.integer().primaryKey({ autoIncrement: true }),
+//   name: p.text().notNull().unique(),
+//   // ✅ 核心：外键字段，关联 usersTable 的 id 主键
+//   userId: p
+//     .integer("user_id")
+//     .notNull()
+//     .unique() // 登录账号必须关联一个用户
+//     .references(() => usersTable.id, {
+//       onDelete: "cascade", // 级联删除：删除用户时，自动删除对应的登录记录
+//       onUpdate: "cascade", // 级联更新：用户 id 变更时，登录表的 userId 同步更新
+//     }),
+//   password: p.text().notNull(),
+//   createdAt: p.integer("created_at", { mode: "timestamp" }),
+// });、
